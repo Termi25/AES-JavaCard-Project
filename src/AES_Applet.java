6ea8e6378df1f3037f8f77b8386ec68bc06f4263
Termi25 <mariusrusu208@gmail.com>
2025-05-05 12:56:29 +0300

    Added additional explanations in code

--- src/AES_Applet.java
+++ src/AES_Applet.java
@@ -5,29 +5,53 @@ import javacardx.crypto.Cipher;
 import java.util.Arrays;
 
 
+/**
+ * AES CBC applet that supports encryption and decryption.
+ *
+ * CLA | Meaning
+ * -----+-----------------------------
+ * 0x00 | ENCRYPT (AES CBC Encryption)
+ * 0x10 | DECRYPT (AES CBC Decryption)
+ * 0x20 | Set IV value (for AES CBC)
+ * 0x30 | Set Key value (for AES CBC)
+ *
+ * INS | Meaning
+ * -----+-----------------------------
+ * 0x10 | P_START (Encrypt/Decrypt 128bit blocks of data with AES CBC)
+ * 0x20 | INS_P_END (Encrypt/Decrypt the last block of data with AES CBC)
+ */
 public class AES_Applet extends Applet {
-    private static final byte[] key = {0x2d, 0x2a, 0x2d, 0x42, 0x55, 0x49, 0x4c, 0x44, 0x41, 0x43, 0x4f, 0x44, 0x45, 0x2d, 0x2a, 0x2d};
-    private static byte[] iv = {0x2d, 0x2a, 0x2d, 0x42, 0x55, 0x49, 0x4c, 0x44, 0x41, 0x43, 0x4f, 0x44, 0x45, 0x2d, 0x2a, 0x2d};
-    protected static Cipher aesCipher;
-    protected static AESKey aesKey;
 
-
+    /*-----Command Constants-----*/
     public static final byte INS_SET_ENCRYPT = 0x00;
     public static final byte INS_SET_DECRYPT = 0x10;
     public static final byte INS_SET_IV = 0x20;
     public static final byte INS_SET_KEY = 0x30;
 
-
     public static final byte INS_P_START = 0x10;
     public static final byte INS_P_END = 0x20;
 
+    /*-----Fields kept in EEPROM-----*/
+    protected static Cipher aesCipher;
+    protected static AESKey aesKey;
+
+    /*-----Algorithm Constants-----*/
+    private static final byte[] key = {0x2d, 0x2a, 0x2d, 0x42, 0x55, 0x49, 0x4c, 0x44, 0x41, 0x43, 0x4f, 0x44, 0x45, 0x2d, 0x2a, 0x2d};
+    private static byte[] iv = {0x2d, 0x2a, 0x2d, 0x42, 0x55, 0x49, 0x4c, 0x44, 0x41, 0x43, 0x4f, 0x44, 0x45, 0x2d, 0x2a, 0x2d};
+    private static final int BYTE_LENGTH = 16;
+
+    /** This is the Class Constructor used for initialising Fields kept in EEPROM.*/
     private AES_Applet() {
+        /* create cipher object */
         aesCipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
+
+        /* create cipher key object */
         aesKey = (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
         aesKey.setKey(key, (short) 0);
         register();
     }
 
+    /** This is the method used when first installing the Applet on the Java Card. */
     public static void install(byte[] bArray, short bOffset, byte BLength) {
         new AES_Applet();
     }
@@ -35,6 +59,9 @@ public class AES_Applet extends Applet {
     @Override
     public void process(APDU apdu) throws ISOException {
         byte[] buffer = apdu.getBuffer();
+
+        /* check if CLA is the one for signing; throws error if not*/
+        /** This is an extra validation for future modification if need be.*/
         switch (buffer[ISO7816.OFFSET_CLA]) {
             case INS_SET_KEY:
                 aesKey.setKey(buffer, ISO7816.OFFSET_CDATA);
@@ -58,28 +85,34 @@ public class AES_Applet extends Applet {
     }
 
     private void setIv(byte[] buffer) {
-        if ((short) buffer[ISO7816.OFFSET_CDATA - 1] != 16) {
+        if ((short) buffer[ISO7816.OFFSET_CDATA - 1] != BYTE_LENGTH) {
             ISOException.throwIt(ISO7816.SW_DATA_INVALID);
         }
         iv = Arrays.copyOfRange(buffer, ISO7816.OFFSET_CDATA, ISO7816.OFFSET_CDATA + 16);
     }
 
     private void setEncrypt() {
-        aesCipher.init(aesKey, Cipher.MODE_ENCRYPT, iv, (short) 0, (short) 16);
+        aesCipher.init(aesKey, Cipher.MODE_ENCRYPT, iv, (short) 0, (short) BYTE_LENGTH);
     }
 
     private void setDecrypt() {
-        aesCipher.init(aesKey, Cipher.MODE_DECRYPT, iv, (short) 0, (short) 16);
+        aesCipher.init(aesKey, Cipher.MODE_DECRYPT, iv, (short) 0, (short) BYTE_LENGTH);
     }
 
     public static void applyAES(APDU apdu) {
+
         byte[] buffer = apdu.getBuffer();
         short len = apdu.setIncomingAndReceive();
+
         switch (buffer[ISO7816.OFFSET_INS]) {
+
+            /* Feeding the first N-1 blocks of data to the AES CBC Cipher Object*/
             case INS_P_START:
                 aesCipher.update(buffer, ISO7816.OFFSET_CDATA, len, buffer, (short) 0x00);
                 apdu.setOutgoingAndSend((short) 0, len);
                 break;
+
+            /* Feeding the last block of data to the AES CBC Cipher Object*/
             case INS_P_END:
                 aesCipher.doFinal(buffer, ISO7816.OFFSET_CDATA, len, buffer, (short) 0x00);
                 apdu.setOutgoingAndSend((short) 0, len);
--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,38 @@ import javax.smartcardio.CommandAPDU;
 import javax.smartcardio.ResponseAPDU;
 import java.io.*;
 
-
+/** An example host for testing the AES_Applet using jCardSim. */
 public class Main {
+
+    /*-----APDU Contract Mode Modifiers (see PQC_Applet)*/
     public static final byte ENCRYPT = 0x00;
     public static final byte DECRYPT = 0x10;
 
     public static void main(String[] args) throws IOException {
+
+        /*-----Card Simulator Creation-----*/
+
         CardSimulator simulator = new CardSimulator();
+
+        /*-----Card Applet Creation, Registration & Selection for use-----*/
 
         AID appletAID = AIDUtil.create("F000000001");
         simulator.installApplet(appletAID, AES_Applet.class);
 
         simulator.selectApplet(appletAID);
 
+        /*-----Initialising Test Folder Root-----*/
+
         File folder = new File("testFiles");
         if(!folder.isDirectory()){
             System.out.println("Test folder does not exists. Add a test folder with this name in project root: testFiles");
             return;
         }
+
+        /*-----Running Applet for each of the test files-----*/
+        /** For each test file it will create:
+         *  1) AES CBC 128 bits with PKCS#7 Padding encrypted file (*.enc);
+         *  2) AES CBC 128 bits with PKCS#7 Padding decrypted file (Decrypted_*). */
 
         for (File file : folder.listFiles()) {
             if (file.isDirectory()) {
@@ -33,8 +47,12 @@ public class Main {
                 continue;
             }
 
+            /*-----Creating file AES encryption using the provided Applet-----*/
+
             File encFile = new File(folder, file.getName().split("\\.")[0] + ".enc");
             encryptFile(simulator, file, encFile, true);
+
+            /*-----Creating file AES decryption using the provided Applet-----*/
 
             File decFile = new File(folder, "Decrypted_" + file.getName());
             encryptFile(simulator, encFile, decFile, false);
@@ -44,15 +62,24 @@ public class Main {
     public static void encryptFile(CardSimulator simulator, File inputFile, File outputFile, boolean isEncrypting) throws IOException {
         if (inputFile.exists()) {
 
+            /*-----Checking Applet Mode-----*/
+
             byte claAESMode = ENCRYPT;
             if (isEncrypting) {
                 claAESMode = DECRYPT;
             }
 
+            /*-----Creating Input Stream-----*/
+
             FileInputStream fis = new FileInputStream(inputFile);
+
+            /*-----Creating Output Stream-----*/
 
             FileOutputStream fos = new FileOutputStream(outputFile);
             BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            /*-----Creating Input Stream Buffer-----*/
+            /** The Offset variable is used to address blocks of bytes from the buffer to be sent to the Java Card. */
 
             byte[] buffer = fis.readAllBytes();
             int offset = 0;
@@ -60,10 +87,14 @@ public class Main {
                 int remainingBytes = buffer.length - offset;
                 boolean lastBlock = (remainingBytes <= 16);
 
-                ResponseAPDU response = null;
+                /*-----Send blocks of bytes to the Java Card for AES CBC processing-----*/
+                /** When reaching the last block of bytes, we need to announce the Java Card
+                 *  so that it may perform the final operations (finishing the AES CBC Encryption or Decryption).*/
                 if (lastBlock) {
                     byte[] lastDataBlock = new byte[16];
                     System.arraycopy(buffer, offset, lastDataBlock, 0, remainingBytes);
+
+                    /*-----Padding with PKCS#7 the last block-----*/
 
                     if (isEncrypting) {
                         int padding = 16 - remainingBytes;
@@ -72,11 +103,14 @@ public class Main {
                                 lastDataBlock[i] = (byte) padding;
                             }
                         }
-                        response = simulator.transmitCommand(new CommandAPDU(claAESMode,0x20,0x00,0x00,lastDataBlock));
+                        ResponseAPDU response = simulator.transmitCommand(new CommandAPDU(claAESMode,0x20,0x00,0x00,lastDataBlock));
                         bos.write(response.getData());
 
                     } else {
-                        response = simulator.transmitCommand(new CommandAPDU(claAESMode,0x20,0x00,0x00,lastDataBlock));
+
+                        /*-----Unpadding with PKCS#7 the last block-----*/
+
+                        ResponseAPDU response = simulator.transmitCommand(new CommandAPDU(claAESMode,0x20,0x00,0x00,lastDataBlock));
 
                         lastDataBlock = response.getData();
 
@@ -90,7 +124,7 @@ public class Main {
                 } else {
                     byte[] dataBlock = new byte[16];
                     System.arraycopy(buffer, offset, dataBlock, 0, 16);
-                    response = simulator.transmitCommand(new CommandAPDU(claAESMode,0x10,0x00,0x00,dataBlock));
+                    ResponseAPDU response = simulator.transmitCommand(new CommandAPDU(claAESMode,0x10,0x00,0x00,dataBlock));
                     bos.write(response.getData());
                 }
                 offset += 16;
